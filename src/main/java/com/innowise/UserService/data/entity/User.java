90167699c65f8cc2c4c7a0016813c41e81ec676e
Anastasiya Oleshko <anastasia.oleshko@innowise.com>
2025-03-17 14:09:39 +0300

    create data layout

---
+++ src/main/java/com/innowise/UserService/data/entity/User.java
@@ -0,0 +1,27 @@
+package com.innowise.UserService.data.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
